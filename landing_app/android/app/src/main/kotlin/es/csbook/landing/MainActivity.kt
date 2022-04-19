37d3112bca64bbfdf937227ee217f1a3ed7d8eaf
Miguel Fuertes <hkfuertes@gmail.com>
2022-04-19 22:49:11 +0200

    initial Commit

---
+++ landing_app/android/app/src/main/kotlin/es/csbook/landing/MainActivity.kt
@@ -0,0 +1,6 @@
+package es.csbook.landing
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ landing_app/android/app/src/main/kotlin/es/csbook/landing_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package es.csbook.landing_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
